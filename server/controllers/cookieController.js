286a4fff403ec0f4a4ec9aa73f4ec7cc30632a0c
PavelKrapivin <pavelkrapivin@Pavels-Air.attlocal.net>
2024-01-25 21:34:41 -0500

    testing dev

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -1,18 +1,41 @@
-const model = require('../models/models.js');
+const model = require("../models/models.js");
+const jwt = require("jsonwebtoken");
+const { setJwtCookie, handleServerError } = require("../utils/functions");
 
-const cookieController = {};
+const cookieControllers = {};
 
-cookieController.setCookie = async (req, res, next) => {
-  const email = req.body.email;
+//Sets session cookie
+cookieControllers.setCookie = async (req, res, next) => {
+  const { email } = req.body;
+  console.log("set cookie req.body->", req.body);
   try {
-    const user = await model.User.findOne({ email });
-    const userID = user._id.toString();
-    console.log('* Setting cookie userID:', userID);
-    res.cookie('id', userID);
+    // const user = await model.User.findOne({ email });
+    // const userID = user._id.toString();
+    // console.log('* Setting cookie userID:', userID);
+    // res.cookie('id', userID);
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    setJwtCookie(res, token);
+    console.log(token);
     return next();
   } catch (err) {
-    console.log('error: ', err);
+    handleServerError(res, err);
   }
 };
 
-module.exports = cookieController;
+// Reset session cookie on logout
+
+cookieControllers.deleteCookie = async (req, res, next) => {
+  try {
+    res.cookie("jwt-cookie", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+    res.status(200).json({ message: "User is logged out" });
+  } catch (err) {
+    handleServerError(res, err);
+  }
+};
+
+module.exports = cookieControllers;
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,11 @@
 const path = require("path");
 const dotenv = require("dotenv");
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // generate an HTML file and inject the necessary script tags automatically
-
 dotenv.config();
-
 module.exports = {
   entry: path.join(__dirname, "./client/src/", "index.js"),
   output: {
+    filename: "bundle.js", // NOTE Specified name 'bundle.js' for when webpack builds app and generates a bundle containing all modules and dependencies
     path: path.resolve(__dirname, "build"),
   },
   mode: process.env.MODE,
@@ -47,7 +46,7 @@ module.exports = {
       directory: path.join(__dirname, "/build"),
       publicPath: "/",
     },
-    hot: true,
+    hot: true, // enables Hot Module Replacement
     historyApiFallback: true,
     compress: true,
     port: process.env.DEV_PORT,
@@ -61,8 +60,8 @@ module.exports = {
     extensions: [".js", ".jsx"],
   },
   performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
+    hints: false, // webpack won't emit warnings based on these size limits during the build
+    maxEntrypointSize: 512000, // sets the max size, in bytes, that the entry point (main bundle) file should be before triggering a warning or error
+    maxAssetSize: 512000, // sets the maximum size for individual assets (like images, stylesheets, etc.)
   },
 };
