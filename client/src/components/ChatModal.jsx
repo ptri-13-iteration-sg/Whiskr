eb554cb0b56fb87d3e2f9d8696a8913c835f8c58
Alex <Alex.D.Dawkins@gmail.com>
2024-01-27 12:20:18 -0500

    MsgBubble added but rendering not working correctly

--- client/src/components/ChatModal.jsx
+++ client/src/components/ChatModal.jsx
@@ -1,10 +1,14 @@
-import React, {useRef, useEffect} from "react";
-import io from 'socket.io-client'
+import React, {useRef, useEffect, useState} from "react";
+import io from 'socket.io-client';
+
+import MsgBubble from "./MsgBubble.jsx";
 
 const socket = io.connect('http://localhost:8080');
 
 const ChatModal = ({open, onClose})=>{
   if(!open) return null;
+
+  const [allMessages, setAllMessages] = useState([]);
 
   const testRoom = 5;
   const inputRef = useRef(null);
@@ -14,7 +18,14 @@ const ChatModal = ({open, onClose})=>{
   }
 
   function sendMessage (inputMsg){
-    socket.emit("send_message", {msg: inputMsg.current.value, testRoom});
+    console.log('SendMsg allMessage:\n', allMessages);
+    const newSendMsg = inputMsg.current.value;
+    allMessages.push(<MsgBubble msg={newSendMsg} />);
+    setAllMessages(allMessages);
+    socket.emit("send_message", {msg: newSendMsg , testRoom});
+
+    
+    console.log(allMessages);
   }
 
   useEffect(()=>{
@@ -23,7 +34,8 @@ const ChatModal = ({open, onClose})=>{
 
   useEffect(() => {
     socket.on('receive_message', (data)=>{
-      alert(data.msg);
+      allMessages.push(<MsgBubble msg={data.msg} />);
+      setAllMessages(data.msg)
     })
   }, [socket])
 
@@ -32,7 +44,9 @@ const ChatModal = ({open, onClose})=>{
     <div className="overlay-chatmodal">
       <div id="chat-container" >
         <button onClick={()=>{socket.off('receive_message');onClose()}}>X</button>
-        <div id="message-container"></div>
+        <div id="message-container">
+          {allMessages}
+        </div>
         <input type="text" ref={inputRef}/>
         <button type="button" onClick={()=>sendMessage(inputRef)}>Send</button>
       </div>
---
+++ client/src/components/MsgBubble.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MsgBubble = ({msg}) => {
+    return(
+      <div className="msgBubble">
+        {msg}
+      </div>
+    )
+}
+
+export default MsgBubble;
