83db01bdef3d117203e7c9d20d9c6e20ca9729b1
PavelKrapivin <pavelkrapivin@Pavels-Air.attlocal.net>
2024-01-27 17:09:52 -0500

    Protected routes added

--- client/src/App.js
+++ client/src/App.js
@@ -10,13 +10,14 @@ import Navbar from "./components/Navbar.js";
 
 // Pages
 import Home from "./pages/Home.js";
-import Login from "./pages/Login.js";
-import Signup from "./pages/Signup.js";
+import Login from "./pages/login.js";
+import Signup from "./pages/signup.js";
 import CreateAccountAdopter from "./pages/CreateAccountAdopter.js";
 import CreateAccountCat from "./pages/CreateAccountCat.js";
 import About from "./pages/About.js";
 import AdopterCardsPage from "./pages/AdopterCardsPage.js";
 import CatCardsPage from "./pages/CatsCardsPage.js";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -31,8 +32,22 @@ function App() {
             <Route path="/signup" element={<Signup />} />
             <Route path="/createAccountAdopter" element={<CreateAccountAdopter />} />
             <Route path="/createAccountCat" element={<CreateAccountCat />} />
-            <Route path="/AdopterCardsPage" element={<AdopterCardsPage />} />
-            <Route path="/CatsCardsPage" element={<CatCardsPage />} />
+            <Route
+              path="/AdopterCardsPage"
+              element={
+                <PrivateRoute>
+                  <AdopterCardsPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/CatsCardsPage"
+              element={
+                <PrivateRoute>
+                  <CatCardsPage />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </div>
       </BrowserRouter>
